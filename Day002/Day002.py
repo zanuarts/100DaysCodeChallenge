ed3da3aeea09c023682afdc428d603ccebdf9a64
zanuarts <nuartea27@gmail.com>
2020-09-10 16:00:37 +0700

    Add Day 002

---
+++ Day002/Day002.py
@@ -0,0 +1,10 @@
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+
+def solution(string):
+    txt = string[::-1]
+    print(txt)
+    return txt
+
+solution('world')
