71529bcd538cace3643e5be98b5f76fdd407d4ed
zanuarts <nuartea27@gmail.com>
2020-09-11 15:07:22 +0700

    Day 3

---
+++ Day003/Day003.py
@@ -0,0 +1,8 @@
+# We need a function that can transform a number into a string.
+
+# What ways of achieving this do you know?
+
+def number_to_string(num):
+    return str(num)
+
+number_to_string(67)
