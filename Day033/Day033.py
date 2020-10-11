00637a537fa4d362dd30018d4cc76a54ae90c994
zanuarts <nuartea27@gmail.com>
2020-10-11 07:49:43 +0700

    Day 33

---
+++ Day033/Day033.py
@@ -0,0 +1,17 @@
+def accum(s):
+    a = []
+    for i, letter in enumerate(s, 1):
+        a.append(i * letter)
+    x = "-".join(a)
+    return x.title()
+
+accum("ZpglnRxqenU") 
+# "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+accum("NyffsGeyylB") 
+# "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+accum("MjtkuBovqrU") 
+# "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+accum("EvidjUnokmM") 
+# "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+accum("HbideVbxncC") 
+# "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
