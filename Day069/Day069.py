eb7594c33f5174553c157e95eea9c442059884e1
zanuarts <nuartea27@gmail.com>
2020-11-16 22:00:11 +0700

    Day 69

---
+++ Day069/Day069.py
@@ -0,0 +1,9 @@
+def get_average(marks):
+    res = sum(marks)/len(marks)
+    return int(res)
+
+# It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+# Return the average of the given array rounded down to its nearest integer.
+
+# The array will never be empty.
