654c974606e3437f80596f79558d64b7cc38a2ba
zanuarts <nuartea27@gmail.com>
2020-11-20 15:25:07 +0700

    day 73

---
+++ Day073/Day073.py
@@ -0,0 +1,15 @@
+def summation(num):
+    res = 0
+    for i in range (0, num+1):
+        res += i
+    return res
+
+# Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+# For example:
+
+# summation(2) -> 3
+# 1 + 2
+
+# summation(8) -> 36
+# 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
