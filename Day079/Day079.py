a44a3baf4ad84e4b8e0b0b5a21d363158f320ac1
zanuarts <nuartea27@gmail.com>
2020-11-26 07:38:58 +0700

    Day 78 & 79

---
+++ Day079/Day079.py
@@ -0,0 +1,19 @@
+def longest(s1, s2):
+    s = s1 + s2
+    res = ''
+    for i in s:
+        if i not in res:
+            res += i
+    res = ''.join(sorted(res))
+    return res
+
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+# each taken only once - coming from s1 or s2.
+# Examples:
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+
+# a = "abcdefghijklmnopqrstuvwxyz"
+# longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
