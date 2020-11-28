672fda17d7b4f3d8da8a76f5869aa9f551186c47
zanuarts <nuartea27@gmail.com>
2020-11-28 10:27:24 +0700

    day 80 and 81

---
+++ Day 081/Day081.py
@@ -0,0 +1,16 @@
+def make_negative( number ):
+    if number < 0:
+        return number
+    return number*-1
+
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+# Example:
+
+# make_negative(1);  # return -1
+# make_negative(-5); # return -5
+# make_negative(0);  # return 0
+# Notes:
+
+# The number can be negative already, in which case no change is required.
+# Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
