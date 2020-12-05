9faecea1ffcc2f2a4a397dff37357ebc36fc5705
zanuarts <nuartea27@gmail.com>
2020-12-05 18:02:29 +0700

    Day 88

---
+++ Day088/Day088.py
@@ -0,0 +1,19 @@
+def odd_or_even(arr):
+    a = sum(arr)
+    if a % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+# Task:
+# Given a list of numbers, determine whether the sum of its elements is odd or even.
+
+# Give your answer as a string matching "odd" or "even".
+
+# If the input array is empty consider it as: [0] (array with a zero).
+
+# Example:
+# odd_or_even([0])          ==  "even"
+# odd_or_even([0, 1, 4])    ==  "odd"
+# odd_or_even([0, -1, -5])  ==  "even"
+# Have fun!
