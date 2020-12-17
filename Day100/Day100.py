d95fbddcceb125ecd8345679bf424382082a4583
zanuarts <nuartea27@gmail.com>
2020-12-17 13:33:00 +0700

    day 100 yeah

---
+++ Day100/Day100.py
@@ -0,0 +1,8 @@
+# Write a function called repeat_str which repeats the given string src exactly count times.
+
+# repeatStr(6, "I") // "IIIIII"
+# repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+
+def repeat_str(repeat, string):
+    return string*repeat
